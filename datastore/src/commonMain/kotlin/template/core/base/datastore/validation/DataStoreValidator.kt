d1b1b589b8a92efcc143592ca7e2f94d14550792
Sk Niyaj Ali <niyaj639@gmail.com>
2025-05-25 07:48:35 +0530

    Delete datastore directory

--- datastore/src/commonMain/kotlin/template/core/base/datastore/validation/DataStoreValidator.kt
+++
@@ -1,9 +0,0 @@
-package template.core.base.datastore.validation
-
-/**
- * Validation layer for data store operations.
- */
-interface DataStoreValidator {
-    fun validateKey(key: String): Result<Unit>
-    fun <T> validateValue(value: T): Result<Unit>
-}
